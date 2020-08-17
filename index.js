91275769516183b9e845a24d38ca3520dd4da96a
osmangonigithub <osmangone939@gmail.com>
2020-08-17 21:47:42 +0600

    almost done

---
+++ index.js
@@ -0,0 +1,80 @@
+const apiUrl="https://api.lyrics.ovh";
+let inputSong=""
+
+document.getElementById('search-btn').addEventListener('click',()=>{
+    inputSong=document.getElementById('input-song').value;
+    if(inputSong.value<1){
+        alert("Please Input Song Name");
+    }
+    else{
+        searchResult();
+    }
+    
+});
+
+
+function searchResult() {
+    fetch(`${apiUrl}/suggest/${inputSong}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const allList=data.data;
+        const firstTen=allList.slice(0,10)
+        setInnerText(firstTen);
+    })
+   
+}
+
+
+function setInnerText(array){
+    for(let i=0;array.length>i;i++){
+        const element=array[i];
+        const title=element.title;
+        const name=element.artist.name;
+        const album=element.album.title
+        document.getElementById(`title-${i}`).innerText=title;
+        document.getElementById(`name-${i}`).innerText=name;
+        document.getElementById(`album-${i}`).innerText=album;
+    }
+}
+document.getElementById('result-wrapper').addEventListener('click',e=>{
+    const targetBtn = e.target;
+    if (targetBtn.tagName === 'BUTTON') {
+        const buttonNumber= parseInt(targetBtn.getAttribute('data-buttonNumber'));
+        clickedBtn(buttonNumber);
+    }
+});
+
+function clickedBtn(buttonNumber){
+     fetch(`${apiUrl}/suggest/${inputSong}`)
+     .then(res=>res.json())
+     .then(data=>{
+        const allList=data.data;
+        const firstTen=allList.slice(0,10);
+        const target=firstTen[buttonNumber];
+        const songTitle=target.title;
+        const artist=target.artist.name;
+        getLyrics(artist,songTitle);
+     })
+    
+}
+
+function getLyrics(artist, songTitle) {
+    fetch(`${apiUrl}/v1/${artist}/${songTitle}`)
+    .then(res=>res.json())
+    .then(data=>{
+        document.getElementById('lyrics-title').innerText=songTitle;
+        const lyrics = data.lyrics;
+        if (lyrics==undefined){
+            console.log("Lyrics is not found (Because Api not provided this lyrics) ")
+            const notFoundMessage="Lyrics is not found (Because Api not provided this lyrics, Please try another.) ";
+            document.getElementById('lyrics').innerText=notFoundMessage;
+        }
+        else{
+            console.log(lyrics);
+            document.getElementById('lyrics').innerText=lyrics;
+        }
+    })
+
+   
+}
+
